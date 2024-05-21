ca24900614bfa5d59a726d1ad37a103b2bea5ff2
Heribert Schütz <hs@webxcerpt.com>
2024-05-21 07:55:13 +0200

    simplified quine slightly
    ...by omitting the "#".
    ("``" suffices as replacement marker.)

--- quine.js
+++ quine.js
@@ -5,7 +5,7 @@ const t = i => `
 const bq = String.fromCharCode(96);
 const d = String.fromCharCode(36);
 const t = i => ${bq}${i}${bq}
-.replace(bq + "#" + bq, d + "{bq}" + d + "{i}" + d + "{bq}");
-console.log(t(t("#")));`
-.replace(bq + "#" + bq, d + "{bq}" + d + "{i}" + d + "{bq}");
-console.log(t(t("#")));
+.replace(bq + bq, d + "{bq}" + d + "{i}" + d + "{bq}");
+console.log(t(t("")));`
+.replace(bq + bq, d + "{bq}" + d + "{i}" + d + "{bq}");
+console.log(t(t("")));
