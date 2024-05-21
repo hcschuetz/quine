6bf870275486de1f2f210b1c3d9d75fb4e51e377
Heribert Schütz <hs@webxcerpt.com>
2024-05-21 14:13:32 +0200

    do replacement earlier

--- quine.js
+++ quine.js
@@ -4,8 +4,6 @@ const d = String.fromCharCode(36);
 const t = i => `
 const bq = String.fromCharCode(96);
 const d = String.fromCharCode(36);
-const t = i => ${bq}${i}${bq}
-.replace(bq + bq, d + "{bq}" + d + "{i}" + d + "{bq}");
-console.log(t(t("")));`
-.replace(bq + bq, d + "{bq}" + d + "{i}" + d + "{bq}");
-console.log(t(t("")));
+const t = i => ${bq}${i}${bq};
+console.log(t(t("").replace(bq + bq, d + "{bq}" + d + "{i}" + d + "{bq}")));`;
+console.log(t(t("").replace(bq + bq, d + "{bq}" + d + "{i}" + d + "{bq}")));
