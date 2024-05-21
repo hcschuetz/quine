bda1e806fc4ef993e1d63efbf2df0d5d46718ad2
Heribert Schütz <hs@webxcerpt.com>
2024-05-21 14:38:25 +0200

    unfolded the dollar constant d

--- quine.js
+++ quine.js
@@ -1,9 +1,7 @@
 
 const bq = String.fromCharCode(96);
-const d = String.fromCharCode(36);
 const t = i => `
 const bq = String.fromCharCode(96);
-const d = String.fromCharCode(36);
 const t = i => ${bq}${i}${bq};
-console.log(t(t("").replace(bq + bq, d + "{bq}" + d + "{i}" + d + "{bq}")));`;
-console.log(t(t("").replace(bq + bq, d + "{bq}" + d + "{i}" + d + "{bq}")));
+console.log(t(t("").replace(bq + bq, "$" + "{bq}$" + "{i}$" + "{bq}")));`;
+console.log(t(t("").replace(bq + bq, "$" + "{bq}$" + "{i}$" + "{bq}")));
